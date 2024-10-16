90c46ab765efd6acb932a9b782cd83149bf0c038
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-16 04:22:39 +0000

    conversion of datatype

--- 01-basic/03-conversion operation.js
+++ 01-basic/03-conversion operation.js
@@ -1,1 +1,31 @@
-console.log("raja singh");
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// conversion of datatype in number
+// "33" => 33
+// "33abc"=> Nan
+// true => 1 ; false => 0 ;
+
+let isloggedIn = "raja singh"
+
+let bolleanIsloogedIn = Boolean(isloggedIn)
+console.log(bolleanIsloogedIn)
+
+// conversion of datatype in Boolean
+
+// 1 => true; 0 => false 
+// "" => false
+// "raja" => true
+
+let someNumber = 54
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
