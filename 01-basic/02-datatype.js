1cf2b6e3e04c8b50be762452c48ce416a7cefec7
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-16 02:52:51 +0000

    all types of datatype

--- 01-basic/02-datatype.js
+++ 01-basic/02-datatype.js
@@ -1,1 +1,19 @@
-console.log("raja singh")
+ "use strict" // treat all code as a newer version of js;
+
+// alert(3+3) we are using nodejs not browser;
+console.log(3+4);
+console.log(3+4);
+
+// Types of DataType
+
+// 1. String = "" 
+// 2. Number => 2 to power 53;
+// 3. Bollean = true or false
+// 4. Null = standalone value 
+// 5.Undefined => 
+// 6.Symbol 
+// 7.Object 
+
+console.log(typeof undefined); // is undefined
+console.log(typeof null); // is Object
+
