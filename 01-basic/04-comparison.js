76269924020073390a4f160e3772b588c5119257
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-17 03:10:24 +0000

    comparison

---
+++ 01-basic/04-comparison.js
@@ -0,0 +1,13 @@
+// console.log("2"< 1);
+// console.log("2">= 1);
+// console.log("2"== 2);
+// console.log("2"!= 1);
+// console.log("2"> 3);
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// === check value and its datatype
+
+console.log("10" === "10")
