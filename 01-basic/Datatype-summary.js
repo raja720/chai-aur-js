e536b7062ec35976db49911353ffd452589dae65
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-20 02:35:44 +0000

    Datatype summary

--- 01-basic/Datatype-summary.js
+++ 01-basic/Datatype-summary.js
@@ -3,35 +3,54 @@
 // There are seven primitive DataType
 // 1.String , 2.Number , 3.Boolean , 4.Null , 5.Undefined , 6.Symbol , 7.BigInt
 
-
-
 // Symbol
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
-console.log(id)
-console.log(anotherId)
-console.log(id === anotherId)
+console.log(id);
+console.log(anotherId);
+console.log(id === anotherId);
 
-const bigNumber = 1234567890987654345676543654367654365n
+const bigNumber = 1234567890987654345676543654367654365n;
 
 // Refrence (Non primitive)
 // Array, Object , Function
 
 // Array
-let heroes = ["saktiman","doremon","spiderman","ironman"];
+let heroes = ["saktiman", "doremon", "spiderman", "ironman"];
 
-// Object 
+// Object
 
 let student = {
-    Name:"raja singh",
-    Roll:20,
-    Age:18,
-    Pass:true
+  Name: "raja singh",
+  Roll: 20,
+  Age: 18,
+  Pass: true,
+};
+console.log(student);
 
-}
-console.log(student)
+let myfunstion = function () {
+  console.log("ram");
+};
 
-let myfunstion = function(){
-    console.log("ram")
+// +++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive) and Heap(Non Primitive)
+
+let myName = "raja bhagat";
+
+let myrealname = myName
+myrealname= "raja Singh"
+console.log(myName)
+console.log(myrealname)
+
+let userOne = {
+    name:"raja singh",
+    email:"rajasingh2004@gmail.com",
+    upi:"kjhgfdbw1234"
 }
+console.log(userOne)
+let userTwo = userOne
+userTwo.name ="raja babu"
+console.log(userTwo)
+console.log(userOne)
