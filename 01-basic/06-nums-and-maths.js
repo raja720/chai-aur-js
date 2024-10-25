1ec039609ea53055ed63573b6ea4c24865edda26
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-25 03:49:45 +0000

    nums-and math

---
+++ 01-basic/06-nums-and-maths.js
@@ -0,0 +1,31 @@
+const score = 100
+const balance = new Number(100)
+
+
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.9836
+
+console.log(otherNumber.toPrecision(3))
+
+const hundred = 1000000000
+
+console.log(hundred.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++++
+
+console.log(Math)
+
+// console.log(Math.abs(-4)) //this function convert negative value in positive
+// console.log(Math.round(4.99))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.99))
+
+// console.log(Math.min(3,2,52,9))
+// console.log(Math.max(3,4,52,9))
+
+
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10+1))
