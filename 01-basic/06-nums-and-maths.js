7d60dc397192b49509fa6311913b1b1300daed50
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-25 16:57:10 +0000

    nums and maths

--- 01-basic/06-nums-and-maths.js
+++ 01-basic/06-nums-and-maths.js
@@ -1,22 +1,21 @@
-const score = 100
-const balance = new Number(100)
+const score = 100;
+const balance = new Number(100);
 
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
-console.log(balance)
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+const otherNumber = 23.9836;
 
-const otherNumber = 23.9836
+console.log(otherNumber.toPrecision(3));
 
-console.log(otherNumber.toPrecision(3))
+const hundred = 1000000000;
 
-const hundred = 1000000000
-
-console.log(hundred.toLocaleString('en-IN'))
+console.log(hundred.toLocaleString("en-IN"));
 
 // ++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++++
 
-console.log(Math)
+console.log(Math);
 
 // console.log(Math.abs(-4)) //this function convert negative value in positive
 // console.log(Math.round(4.99))
@@ -26,6 +25,10 @@ console.log(Math)
 // console.log(Math.min(3,2,52,9))
 // console.log(Math.max(3,4,52,9))
 
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10 + 1));
 
-console.log(Math.random()*10+1)
-console.log(Math.floor(Math.random()*10+1))
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
