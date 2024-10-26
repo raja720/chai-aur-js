5d0f4911d6c0c1adecda46726ef28745811c1f00
Raja singh <162097745+raja720@users.noreply.github.com>
2024-10-26 03:28:26 +0000

    dates. in js

---
+++ 01-basic/dates.js
@@ -0,0 +1,11 @@
+// let dates = new Date()
+// console.log(typeof dates)
+// console.log(dates.toString());
+// console.log(dates.toDateString());
+// console.log(dates.toISOString());
+// console.log(dates.toJSON());
+// console.log(dates.toLocaleDateString());
+// console.log(dates.toLocaleString());
+
+let myCreatedDate = new Date(2024,0,12)
+console.log(myCreatedDate.toDateString())
