4fe2492199c16495503b8d5e0d9f112648b21f5e
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-02 03:41:03 +0000

    array

---
+++ 02-basic/01-Array.js
@@ -0,0 +1,33 @@
+let myArr = ["raja",3,5,true,"love","shriti"]
+// console.log(myArr);
+
+const myArr1=new Array(1,2,3,4,5,"raja")
+// console.log(myArr1);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(5)
+// myArr.pop()
+
+// myArr.unshift(10)
+// myArr.shift()
+
+// console.log(myArr.includes("shriti"));
+// console.log(myArr.indexOf("shriti"));
+
+// let joinArr = [2,4,5,345]
+// const newArr = myArr.join()
+// console.log(newArr);
+
+console.log( 'A' ,myArr);
+console.log(myArr.slice(1,4));
+
+console.log(myArr.splice(1,3));
+
+
+
+
+
+
+// console.log(myArr);
