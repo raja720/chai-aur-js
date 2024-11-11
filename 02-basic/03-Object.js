40a27992917553913f901afd1e52bdf410564e56
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-11 01:36:52 +0000

    object

--- 02-basic/03-Object.js
+++ 02-basic/03-Object.js
@@ -1,7 +1,7 @@
 // Object ;
 
 //singleton
-//object.create
+//object.create {constructor method under this method sigle ton create}
 
 // USing symbol in a object
 
@@ -18,16 +18,30 @@ let JsUser = {
     lastloggedIndays:["monday,wednesday,friday"]
 }
 
-console.log(JsUser);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser.email); //this is first method for print the array;
+// console.log(JsUser["email"]); //this is another method for print the array;
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
 
 JsUser.email = "rajayahoo@gamil.com"
-console.log(JsUser["email"]);
+// console.log(JsUser["email"]);
 
-Object.freeze(JsUser) // use this method for freeze the object
+// Object.freeze(JsUser) // use this method for freeze the object
 JsUser.email = "rajaygolo@gamil.com"
-console.log(JsUser["email"]);
+// console.log(JsUser["email"]);
 
-console.log(JsUser);
+// console.log(JsUser);
+
+JsUser.greetings = function () {
+    console.log("Hello js user");
+    
+}
+JsUser.greetingtwo = function () {
+    console.log(`Hello js user , ${this.name}`);
+    
+}
+
+console.log(JsUser.greetingtwo());
+
+console.log(JsUser.greetings());
+
