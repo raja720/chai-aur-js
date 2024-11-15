1c23809a1a6d58b4ed62e1ae80bc452cffb4b231
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-15 06:20:05 +0000

    functionsss

--- 03-basic/01-functions.js
+++ 03-basic/01-functions.js
@@ -27,3 +27,37 @@ function loginUsermsg(username = "sam"){ //sam use for default parameter
 }
 
 console.log(loginUsermsg(''))
+
+function calculatecartPrice(val1,val2,...num1) {
+    return num1;
+
+
+}
+// console.log(calculatecartPrice(200,400,500,1000));
+
+const user = {
+    name : "raja",
+    coursename :"js hindi",
+    price : 1999
+
+}
+
+function coursedetail (anyobject){
+    console.log(`user name is ${anyobject.name} and price is ${anyobject.price}`);
+    
+
+}
+
+coursedetail({
+    name:"babulaal",
+    price: "2lakh"
+})
+
+const newarray = [200,300,400,500];
+
+function returnArrayValue(getArray) {
+    return(getArray[3])
+}
+
+// console.log(returnArrayValue(newarray));
+console.log(returnArrayValue([200,300,400,50000]));
