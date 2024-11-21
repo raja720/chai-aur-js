55fcc44c746f70c85fb5df9a948404d9bbe1934f
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-21 03:23:11 +0000

    tuthy

---
+++ 04-control.flow/truthy.js
@@ -0,0 +1,17 @@
+let userEmail = "hsfiuerrbfuysf";
+
+if(userEmail){
+    console.log("user is loggedIn");
+    
+}else{
+    console.log("don't have user email");
+    
+}
+
+// falsy value 
+
+false, 0 ,-0 , null, NaN, "" , BigInt 0n , undefined
+
+// truthy vlaue 
+
+[], "0", " ", {}, "flase",function(){} ,
