4591e494adaf9fbe1affd3579a0626740dd75942
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-21 02:55:51 +0000

    contol flow

---
+++ 04-control.flow/one.js
@@ -0,0 +1,41 @@
+// if statment 
+const userage = 17;
+const temperature = 51;
+if (temperature < 50) {
+    console.log("temperature  is 50");
+
+}
+console.log("temperature is less than 50");
+
+// const score= 200;
+
+// if(score>100){
+//     let power = "fly"
+//     console.log(`user power ${power}`);
+    
+// }
+// console.log(`user power ${power}`);
+
+
+
+const balance = 1000;
+
+// if(balance>500) console.log("you are selected"),console.log("test2"); 
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedinFromgoogle = true;
+const loggedinFromemail = true;
+
+
+if (userLoggedIn&&debitCard && 2== 2){
+    console.log("allow to buy course");
+    
+}
+if (loggedinFromgoogle||loggedinFromemail){
+    console.log("you are logged in");
+    
+}
+
+
+// < , > , <= , >= , == , != , === ,!==, 
---
+++ 04-control.flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let userage = 18 
+let month = "april";
+switch (month) {
+    case "jan":
+        console.log("user month is jan");
+        
+        break;
+    case "feb":
+        console.log("user month is feb");
+        
+        break;
+    case "march":
+        console.log("user month is march");
+        
+        break;
+    case "april":
+        console.log("user month is april");
+        
+        break;
+    case "may":
+        console.log("user month is may");
+        
+        break;
+
+    default:
+        break;
+}
