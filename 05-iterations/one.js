e62a50b244905877a296f845bc25811041e96242
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-26 13:20:16 +0000

    iterations day 1

--- 05-iterations/one.js
+++ 05-iterations/one.js
@@ -1,1 +1,58 @@
+//for loop
 
+for (let i = 0; i <=10; i++) {
+    const element = i
+    if (element == 5) {
+        console.log(`${element} is the middle number`);
+        
+    }
+    console.log(element);
+        
+}
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value is ${i} `);
+    
+    for (let j = 1; j <= 10; j++) {
+    //    console.log(`inner loop value is ${j} and outer loop value is ${i}`);
+    
+    // console.log(` ${i}*${j} = ${i*j}`);
+    
+       }
+    
+
+
+}
+
+let myArray = ["raja" , "baja" , "khaja"];
+console.log(myArray.length);
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+    
+    
+}
+
+// break and continue 
+
+// for (let i = 1; i <= 20; i++) {
+//     const element = i;
+//     if (element == 10) {
+//         console.log(`${element} is middle number  `);
+//         break
+//     }
+//     console.log(element);
+    
+    
+// }
+
+for (let i = 1; i <= 20; i++) {
+    
+    if (i == 10) {
+        console.log(`${i} is middle number  `);
+        continue
+    }
+    console.log(i);
+    
+    
+}
