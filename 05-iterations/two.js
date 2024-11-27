3b8990be6b0004ff4e26fb4a3850fb417e50049f
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-27 13:02:38 +0000

    two .js

---
+++ 05-iterations/two.js
@@ -0,0 +1,23 @@
+// // while and do while loop 
+// let index = 10;
+// while (index <= 100) {
+//     console.log(`vlaue of index is ${index} `);
+//     index = index+2
+// }
+
+let myArray = ["batman","janeman",'Kareja'];
+
+// let arr= 0;
+// while (arr < myArray.length) {
+//     console.log(`value of arr is ${myArray[arr]}`);
+//     arr++
+// }
+
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+
+} while (score <= 20);
