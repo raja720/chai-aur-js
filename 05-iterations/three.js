006b0c6638cbffd434eb365fbf09b83ad2e520a8
Raja singh <162097745+raja720@users.noreply.github.com>
2024-11-28 03:08:07 +0000

    for of loop

---
+++ 05-iterations/three.js
@@ -0,0 +1,45 @@
+// for of 
+// ["","",""]
+// [{},{},{},{}]
+
+let array = [1,2,3,4,5,6,7,8,9];
+
+for (const num of array) {
+    console.log(num);
+    
+}
+
+const greetings = "hello world"
+
+for (const i of greetings) {
+    console.log(`each char is ${i}`);
+    
+}
+
+// Mpas 
+
+const map =new Map()
+map.set("IN","India")
+map.set("USA","United State of America")
+map.set("FR","France")
+// map.set("IN","India") map is known as its unique behaviour it can print unique value 
+
+// console.log(map);
+
+for (const [key , value] of map) {
+    console.log(key , value);
+    
+}
+
+const myObject = {
+    "Game1":"free fire",
+    "Game2":"Pubg",
+    "Game3":"khelega"
+
+}
+// for of loops doesn'nt worling with object because object is nor iterable 
+                                //   88888
+// for (const [key , value] of myObject) {
+//     console.log(key ,value);
+    
+// }
