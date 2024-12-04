156828922d1527be813d7343c43f2484e30ef18b
Raja singh <162097745+raja720@users.noreply.github.com>
2024-12-04 01:53:37 +0000

    filter method

--- 05-iterations/six.js
+++ 05-iterations/six.js
@@ -28,7 +28,41 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // console.log(newNums);
 
 
-const myself=myNums.filter((num)=>{
-    return num > 4
+// const myself=myNums.filter((num)=>{
+//     return num > 4
+// })
+// console.log(myself);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+//   let newnums = []
+
+//   books.forEach((bk)=>{
+//     if (bk.publish === 1986) {
+//         newnums.push(bk)
+//     }
+//   })
+//   console.log(newnums);
+
+//  let newbk =books.filter((bk)=> bk.publish === 2009)
+//   console.log(newbk);
+  
+//     let newbk =books.filter((bk)=> {
+//         return bk.publish === 2009})
+//   console.log(newbk);
+  
+let newbk = books.filter((bk)=> {
+    return bk.publish >= 1900 && bk.genre === "History"
 })
-console.log(myself);
+console.log(newbk);
