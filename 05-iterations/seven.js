fe91e57abb018aa9bfecafc5c81b0fc88e828151
Raja singh <162097745+raja720@users.noreply.github.com>
2024-12-06 03:02:05 +0000

    map and filter method in js

--- 05-iterations/seven.js
+++ 05-iterations/seven.js
@@ -1,4 +1,11 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10];
 
-const newNums = myNums.map((num)=>{return num *10 }).map((num)=> {return num*2})
+// const newNums = myNums.map((num)=>{return num *10 })
+
+//chaining:-
+const newNums = myNums.map((num)=> num *10 )
+.map((num)=> num + 30)
+.filter((num)=>num>= 80)
+
+
 console.log(newNums);
