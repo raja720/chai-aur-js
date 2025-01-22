3770e2ac07c2b0da5c64d30af8cc837163fde329
Raja singh <162097745+raja720@users.noreply.github.com>
2025-01-22 04:49:51 +0000

    promies

--- 09-advance/promises.js
+++ 09-advance/promises.js
@@ -1,82 +1,44 @@
-fetch('https://something.com').then().finally().catch()
-
-const promiseOne = new Promise((resolve, reject) => {
-    // Do any Async task 
-    // Db call , Cryptography , network call
+const promiseOne = new Promise(( resolve , reject )=>{
+    // do an async task 
     setTimeout(() => {
-        console.log('async task is complete');
+        console.log("raja singh is the noob");
         resolve()
     }, 1000);
 
 })
-
-promiseOne.then(() => {
-    console.log("Promise consumed");
-})
-
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('async2 task is completed');
-        resolve()
-    }, 1000);
-}).then(() => {
-    console.log("async 2 resolved");
-})
-
-const promiseThree = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve({ Name: "raja", email: "rajababu@gmail.com" })
-    }, 1000);
-})
-promiseThree.then((user) => {
-    console.log(user);
-})
-
-
-const promiseFour = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const error = false
-        if (!error) {
-            resolve({ username: "Raja singh", pass: "12345" })
-        } else {
-            reject(" ERROR: something went wrong")
-        }
-    }, 1000);
-})
-
-promiseFour.then((user) => {
-    console.log(user);
-    return user.pass
-})
-.then((username) => {
-    console.log(username);
-})
-.catch((error) => {
-    console.log(error);
-})
-.finally(()=>{
-    console.log("the promise is either resolved or reject");
-})
-
-
-const promiseFive = new Promise((resolve ,reject)=>{
-    setTimeout(() => {
-        const error = false
-        if (!error) {
-            resolve({ username: "JavaScript", pass: "123345" })
-        } else {
-            reject(" ERROR: JS went wrong")
-        }
-    }, 1000);
-})
-
-
-
-// console.log("adsfuje");
-
-// setTimeout(() => {
-//     console.log("raja singh");   call stack example
+promiseOne.then(()=>{
+    console.log("promise consumed");
     
-// }, );
+})
 
-// console.log("ksudg");
+new Promise((resolve , reject)=>{
+    setTimeout(() => {
+        console.log("async task 2");
+        resolve()
+    }, 1000);
+
+}).then(()=>{
+    console.log("async task 2 completed");
+    
+})
+
+const promiseThree = new Promise((resolve ,reject )=>{
+    setTimeout(() => {
+        console.log("async task 3");
+        resolve({username:"chai" , email: "rajababu@gmail.com"})
+        
+    }, 2000);
+
+})
+
+promiseThree.then((user)=>{
+    console.log(user);
+    
+})
+
+
+const promiseFour = new Promise(()=>{
+    setTimeout(() => {
+        
+    }, 1000);
+})
